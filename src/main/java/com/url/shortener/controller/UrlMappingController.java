4099df34cf14a39ac764c4e07fc4d89afaa37957
shubham kumar <shubham27034@gmail.com>
2026-02-03 00:39:57 +0530

    Fetching all URLs of the USer

--- src/main/java/com/url/shortener/controller/UrlMappingController.java
+++ src/main/java/com/url/shortener/controller/UrlMappingController.java
@@ -24,14 +24,15 @@ public class UrlMappingController {
     private UserService userService;
 
     //{"originalUrl":"https://example.com"}
+    // https://abc.com/UfxskyKa -->https:/example.com
 
     @PostMapping("/shorten")
-    @PreAuthorize("hasRole('User)")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<UrlMappingDTO> createShortUrl(@RequestBody Map<String, String> request,
                                                         Principal principal){
         String originalUrl = request.get("originalUrl");
       User user = userService.findByUsername(principal.getName());
-        UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl);
+        UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl, user);
         return ResponseEntity.ok(urlMappingDTO);
     }
 }
