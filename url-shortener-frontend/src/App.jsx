980d3d0214de7ffb88946fe23eea060076a4f1a7
shubham kumar <shubham27034@gmail.com>
2026-02-05 00:06:02 +0530

    frontend added and tailwind css setup

---
+++ url-shortener-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <dev><h1 class="text-3xl font-bold underline">
+      Hello shubham!
+    </h1></dev>
+    </>
+  )
+}
+
+export default App
